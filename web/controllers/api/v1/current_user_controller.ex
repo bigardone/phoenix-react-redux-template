64fd3b9839a4d2777b3d174e59f705069992288e
Ricardo García <bigardone@gmail.com>
2016-03-23 09:29:10 +0100

    Removed unnecessary function

--- web/controllers/api/v1/current_user_controller.ex
+++ web/controllers/api/v1/current_user_controller.ex
@@ -4,24 +4,10 @@ defmodule HelloPhoenix.CurrentUserController do
   plug Guardian.Plug.EnsureAuthenticated, handler: HelloPhoenix.SessionController
 
   def show(conn, _) do
-    case decode_and_verify_token(conn) do
-      { :ok, _claims } ->
-        user = Guardian.Plug.current_resource(conn)
-
-        conn
-        |> put_status(:ok)
-        |> render("show.json", user: user)
-
-      { :error, _reason } ->
-        conn
-        |> put_status(:not_found)
-        |> render(HelloPhoenix.SessionView, "error.json", error: "Not Found")
-    end
-  end
-
-  defp decode_and_verify_token(conn)  do
+    user = Guardian.Plug.current_resource(conn)
+    
     conn
-    |> Guardian.Plug.current_token
-    |> Guardian.decode_and_verify
+    |> put_status(:ok)
+    |> render("show.json", user: user)
   end
 end
